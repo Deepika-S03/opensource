eb652dd61cc06467153f64f7907e1761ccee6850
Deepika-S03 <yedladeepika0@gmail.com>
2024-11-14 22:20:42 +0530

    Create R_P_S.py

---
+++ visaa_prep/R_P_S.py
@@ -0,0 +1,8 @@
+V,C=input().split()
+if (V=='R' and C=='S') or (V=='P' and C=='R') or (V=='S' and C=='P'):
+    print("Vignesh")
+else:
+    if V==C:
+        print("NULL")
+    else:
+        print("Charan")
